fa5479a2703d18379a676d2cae1c4fca33e5eb38
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-04-05 20:16:49 +0900

    feat: 필터 생성 레이아웃

--- lib/screens/filtter/add_filter_screen.dart
+++ lib/screens/filtter/add_filter_screen.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:gyenongrak_app/widgets/filter/filter_form.dart';
 
 class AddFilterScreen extends StatelessWidget {
   const AddFilterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('필터 생성', style: TextStyle(fontWeight: FontWeight.w600)),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('필터 생성', style: TextStyle(fontWeight: FontWeight.w600)),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [_MarketArea(), _ProductArea(), _FilterSaveButton()],
+          ),
+        ),
       ),
-      body: Row(children: []),
+    );
+  }
+}
+
+class _MarketArea extends StatelessWidget {
+  const _MarketArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: 5, child: Container(child: Container()));
+  }
+}
+
+class _ProductArea extends StatelessWidget {
+  const _ProductArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: 10, child: Container());
+  }
+}
+
+class _FilterSaveButton extends StatelessWidget {
+  const _FilterSaveButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width / 1.5,
+          child: ElevatedButton(onPressed: () {}, child: const Text('생성')),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/filter/filter_drop_down_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FilterDropDownButton extends StatelessWidget {
+  const FilterDropDownButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text('저장 하기'));
+  }
+}
