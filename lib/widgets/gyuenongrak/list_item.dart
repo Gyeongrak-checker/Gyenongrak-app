3401fde938087e021d2bcf7d6108c4f59f442215
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-04-11 19:40:56 +0900

    fix: UI 수정

--- lib/widgets/gyuenongrak/list_item.dart
+++ lib/widgets/gyuenongrak/list_item.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class FilterListItem extends StatelessWidget {
-  // final DateTime dateTime;
-  // final String productName;
-  // final String region;
+  final DateTime dateTime;
+  final String productName;
+  final String region;
+  final String tradingVolume;
+  final String type;
+  final String value;
 
   const FilterListItem({
     super.key,
-    // required this.dateTime-
+    required this.dateTime,
+    required this.productName,
+    required this.region,
+    required this.tradingVolume,
+    required this.type,
+    required this.value,
   });
 
   // 경매시간, 품목, 산지, 거래량, 규격, 경락가
@@ -15,6 +23,8 @@ class FilterListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String date = '${dateTime.day}일 ${dateTime.hour}시  ${dateTime.minute}분';
+
     return Container(
       height: 64,
       margin: EdgeInsets.all(8.0),
@@ -23,15 +33,30 @@ class FilterListItem extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('hello'),
-          Text('hello'),
-          Text('hello'),
-          Text('hello'),
-          Text('hello'),
-          Text('hello'),
+          _Item(child: date),
+          _Item(child: productName),
+          _Item(child: region),
+          _Item(child: tradingVolume),
+          _Item(child: type),
+          _Item(child: "$value원"),
         ],
       ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  final String child;
+
+  const _Item({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Text(child),
     );
   }
 }
