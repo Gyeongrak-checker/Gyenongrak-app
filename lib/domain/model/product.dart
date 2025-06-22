b1624b66652e3c9203302a390b168a737051258e
yuseungdo <yus174113@gmail.com>
2025-06-23 00:10:04 +0900

    Product 모델 추가

---
+++ lib/domain/model/product.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product.freezed.dart';
+part 'product.g.dart';
+
+@freezed
+class Product with _$Product {
+  const factory Product({
+    required DateTime auction,
+    required String productName,
+    required String originPlace,
+    required String weight,
+    required String package,
+    required int value,
+  }) = _Product;
+
+  factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
+}
